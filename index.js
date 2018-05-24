74c1ca98a4a85ccc513d5208ed14e5b0bcabc131
James Ferguson <ncjamesferguson@gmail.com>
2018-05-24 18:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,4 +36,12 @@ class Customer {
     this.id = ++customerId;
     store.customers.push(this);
   }
+
+  deliveries(){
+    return store.deliveries.filter(delivery => delivery.customerId === this.id)
+  }
+
+  meals(){
+    return this.deliveries().map(delivery => delivery.meal())
+  }
 }
